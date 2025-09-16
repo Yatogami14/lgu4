d2b768fef3cc2fc163ebbb6543ff7855f088d9e6
Yatogami014 <jeffparay014@gmail.com>
2025-09-17 05:09:05 +0800

    Initial commit

--- replace_db_core.bat
+++ replace_db_core.bat
@@ -1,12 +1,13 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "search=new User($db_core)"
+REM The search string needs to have regex special characters like ( ) and $ escaped for PowerShell's -replace operator.
+set "search=new User\(\$db_core\)"
 set "replace=new User($database)"
 
 for /r "c:\xampp\htdocs\lgu4" %%f in (*.php) do (
     if exist "%%f" (
-        powershell -Command "(Get-Content '%%f') -replace '%search%', '%replace%' | Set-Content '%%f'"
+        powershell -Command "(Get-Content '%%f' -Raw) -replace '%search%', '%replace%' | Set-Content '%%f'"
     )
 )
 
