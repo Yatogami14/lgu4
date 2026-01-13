9b075f0684fa483ffff2d3324b0250bdc0fa31d9
Yatogami14 <jeffparay014@gmail.com>
2026-01-13 17:40:25 +0800

    Initial commit

--- config/database.php
+++ config/database.php
@@ -8,10 +8,10 @@ class Database {
     public function __construct() {
         $this->config = [
             self::DB_UNIFIED => [
-                'host' =>  'localhost:3307', //getenv('DB_HOST') ?:
-                'dbname' => 'hsi_lgu_unified', //getenv('DB_NAME') ?:
-                'username' => 'root', //getenv('DB_USER') ?:
-                'password' => '', //getenv('DB_PASS') ?:
+                'host' =>  '153.92.15.81', 
+                'dbname' => 'u514031374_hsi', 
+                'username' => 'u514031374_hsi', 
+                'password' => 'hsiP@55w0rd', 
             ]
         ];
     }
